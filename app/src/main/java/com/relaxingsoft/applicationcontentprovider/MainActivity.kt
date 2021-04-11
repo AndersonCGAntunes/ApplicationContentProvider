0acf39e5b2a551c1cac078238694724aa5543ce2
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-04-11 11:30:18 -0300

    CardView com ConstraintLayout created

--- app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
+++ app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.relaxingsoft.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks< Cursor 
     lateinit var noteRecyclerView: RecyclerView
     lateinit var noteAdd: FloatingActionButton
 
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +25,12 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks< Cursor 
         noteAdd = findViewById( R.id.note_add )
         noteAdd.setOnClickListener{  }
 
+        adapter = NotesAdapter()
+        adapter.setHasStableIds( true )
+
         noteRecyclerView = findViewById( R.id.notes_recycler )
+        noteRecyclerView.layoutManager = LinearLayoutManager( this )
+        noteRecyclerView.adapter = adapter
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = CursorLoader( this, URI_NOTES, null, null, null, "TITLES_NOTE" )
