eaa2eb607d217bc26e12c6f06cb62232933b2a45
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-04-11 10:54:03 -0300

    CoordinatorLayout created

--- app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
+++ app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.relaxingsoft.applicationcontentprovider
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.relaxingsoft.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks< Cursor > {
+
+    lateinit var noteRecyclerView: RecyclerView
+    lateinit var noteAdd: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteAdd = findViewById( R.id.note_add )
+        noteAdd.setOnClickListener{  }
+
+        noteRecyclerView = findViewById( R.id.notes_recycler )
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = CursorLoader( this, URI_NOTES, null, null, null, "TITLES_NOTE" )
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if ( data != null ) {
+
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
