0043ff3e548947814866aafdec603468dd2a5093
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2021-04-12 12:40:18 -0300

    Complementando a classe NotesDetailFragment

--- app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
+++ app/src/main/java/com/relaxingsoft/applicationcontentprovider/MainActivity.kt
@@ -26,11 +26,15 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks< Cursor 
         setContentView(R.layout.activity_main)
 
         noteAdd = findViewById( R.id.note_add )
-        noteAdd.setOnClickListener{  }
+        noteAdd.setOnClickListener{
+            NotesDetailFragment().show( supportFragmentManager, "dialog" )
+        }
 
         adapter = NotesAdapter( object: NoteClickedListener {
             override fun noteClickedItem(cursor: Cursor) {
-                val id = cursor.getLong( cursor.getColumnIndex( _ID ) )
+                val id: Long = cursor.getLong( cursor.getColumnIndex( _ID ) )
+                val fragment: NotesDetailFragment = NotesDetailFragment.newInstance( id )
+                fragment.show( supportFragmentManager, "dialog" )
             }
 
             override fun noteRemoveItem(cursor: Cursor?) {
@@ -44,17 +48,17 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks< Cursor 
         noteRecyclerView = findViewById( R.id.notes_recycler )
         noteRecyclerView.layoutManager = LinearLayoutManager( this )
         noteRecyclerView.adapter = adapter
+
+        LoaderManager.getInstance( this ).initLoader( 0, null, this )
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = CursorLoader( this, URI_NOTES, null, null, null, TITLE_NOTES )
 
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        if ( data != null ) {
-
-        }
+        if ( data != null ) { adapter.setCursor( data ) }
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        adapter.setCursor( null )
     }
 }
--- app/src/main/java/com/relaxingsoft/applicationcontentprovider/NotesDetailFragment.kt
+++ app/src/main/java/com/relaxingsoft/applicationcontentprovider/NotesDetailFragment.kt
@@ -1,11 +1,19 @@
 package com.relaxingsoft.applicationcontentprovider
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ContentValues
 import android.content.DialogInterface
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.relaxingsoft.applicationcontentprovider.database.NotesDatabaseHelper.Companion.DESCRIPTION_NOTES
+import com.relaxingsoft.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
+import com.relaxingsoft.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
 
 class NotesDetailFragment: DialogFragment(), DialogInterface.OnClickListener {
 
@@ -31,11 +39,39 @@ class NotesDetailFragment: DialogFragment(), DialogInterface.OnClickListener {
         noteEditTitle = view?.findViewById( R.id.note_edt_title ) as EditText
         noteEditDescription = view.findViewById( R.id.note_edt_description ) as EditText
 
-        return super.onCreateDialog(savedInstanceState)
+        var newNote = true
+        if ( arguments != null && arguments?.getLong( EXTRA_ID ) != 0L ) {
+            id = arguments?.getLong( EXTRA_ID ) as Long
+            val uri: Uri = Uri.withAppendedPath( URI_NOTES, id.toString() )
+            val cursor: Cursor? = activity?.contentResolver?.query( uri, null, null, null, null )
+            if ( cursor?.moveToNext() as Boolean ) {
+                newNote = false;
+                noteEditTitle.setText( cursor.getString( cursor.getColumnIndex( TITLE_NOTES ) ) )
+                noteEditDescription.setText( cursor.getString( cursor.getColumnIndex( DESCRIPTION_NOTES ) ) )
+            }
+            cursor.close()
+        }
+
+        return AlertDialog.Builder( activity as Activity )
+                .setTitle( if ( newNote ) "Nova mensagem" else "Editar mensagem" )
+                .setView( view )
+                .setPositiveButton( "Salvar", this )
+                .setNegativeButton( "Cancelar", this )
+                .create()
     }
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
-        TODO("Not yet implemented")
+        val values = ContentValues()
+        values.put( TITLE_NOTES, noteEditTitle.text.toString() )
+        values.put( DESCRIPTION_NOTES, noteEditDescription.text.toString() )
+
+        if ( id != 0L ) {
+            val uri = Uri.withAppendedPath( URI_NOTES, id.toString() )
+            context?.contentResolver?.update( uri, values, null, null )
+        } else {
+            context?.contentResolver?.insert( URI_NOTES, values )
+        }
+
     }
 
 }
